f7a0053c23f90848f7a1524c396f2b8847f3977d
Caio souza lima <caiosouzalima2002@gmail.com>
2022-11-21 18:09:04 -0300

    feat: sidebar list is connect with api

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,22 +1,33 @@
 import {Card} from "@mui/material";
 import {Column} from "./column";
 import Link from "next/link";
+import React, {useEffect} from "react";
 
 
 export const Sidebar = () => {
+  const [jogos, setJogos] = React.useState([]);
+
+  useEffect(() => {
+    fetch(
+      'https://champstats.purpleglacier-6a593a11.eastus.azurecontainerapps.io/jogos/', {method: 'GET', mode: 'cors'}
+    )
+      .then((response) => response.json())
+      .then((data) => setJogos(data));
+  });
+
   const StyleGamesName = ({
     color: '#fff',
     fontSize: '1rem',
+    cursor: 'pointer',
   });
+
   return (
     <Card sx={{display:'flex', flexDirection:'column', backgroundColor:'#252525', width:'20rem', alignItems:'center'}}>
         <Link href={'/'}><p style={{color:'#fff', fontSize:'2rem'}}>Jogos</p></Link>
       <Column padding={'1rem'}>
-        <Link href={'/campeonato'}><div style={StyleGamesName}>League of Legends</div></Link>
-        <Link href={'/campeonato'}><div style={StyleGamesName}>Cs:Go</div></Link>
-        <Link href={'/campeonato'}><div style={StyleGamesName}>Dota</div></Link>
-        <Link href={'/campeonato'}><div style={StyleGamesName}>Dota 2</div></Link>
-        <Link href={'/campeonato'}><div style={StyleGamesName}>Valorant</div></Link>
+        {jogos.map(({_id, nome}, index) => (
+          <div onClick={() => console.log(`ID: ${_id}`)} key={index} style={StyleGamesName}>{nome}</div>
+        ))}
       </Column>
     </Card>
   );
